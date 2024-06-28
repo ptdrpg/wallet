853ab7811b12ce5e9c3f6ee8f87cd603245ff4bf
ptdrpg <pagielamizael@gmail.com>
2024-06-28 10:14:51 +0300

    fix lint blockchain

--- blockchain/addBlock.go
+++ blockchain/addBlock.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ptdrpg/wallet/entity"
 )
 
-func History (data Block) error {
+func History(data Block) error {
 	outputDir := "upload/transaction/"
 	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
 		err := os.MkdirAll(outputDir, 0755)
@@ -31,22 +31,25 @@ func History (data Block) error {
 		return errors.New(err.Error())
 	}
 
-	WriteFile:= os.WriteFile(filepath, newData, 0755)
-	if WriteFile != nil{
+	WriteFile := os.WriteFile(filepath, newData, 0755)
+	if WriteFile != nil {
 		return errors.New(WriteFile.Error())
 	}
 
 	return nil
 }
 
-func(b *Blockchain) AddBlock(data entity.Transaction) {
+func (b *Blockchain) AddBlock(data entity.Transaction) {
 	lastBlock := b.Chain[len(b.Chain)-1]
 	newBlock := Block{
 		PreviousHash: lastBlock.Hash,
-		Data: data,
-		TimeStampe: time.Now(),
+		Data:         data,
+		TimeStampe:   time.Now(),
 	}
 	newBlock.Mine(b.Difficulty)
-	History(newBlock)
+	History(Block{
+		PreviousHash: newBlock.PreviousHash,
+		Data:         newBlock.Data,
+	})
 	b.Chain = append(b.Chain, newBlock)
 }
